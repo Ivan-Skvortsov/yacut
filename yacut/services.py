f01086aef1a2318013951549bbfe33a655309d79
Ivan Skvortsov <pprofcheg@gmail.com>
2022-07-23 20:24:00 +0300

    refactored services.get_unique_short_id

--- yacut/services.py
+++ yacut/services.py
@@ -4,14 +4,14 @@ import random
 from yacut.models import URL_map
 
 
+def short_id_exists_in_db(short_id):
+    """Checks if given short_id already extists in database."""
+    return URL_map.query.filter_by(short=short_id).first() is not None
+
+
 def get_unique_short_id():
     """Random unique string generator with digits and letters."""
     while True:
         id = ''.join(random.choices(string.digits + string.ascii_letters, k=6))
-        if URL_map.query.filter_by(short=id).first() is None:
+        if not short_id_exists_in_db(id):
             return id
-
-
-def short_id_exists_in_db(short_id):
-    """Checks if given short_id already extists in database."""
-    return URL_map.query.filter_by(short=short_id).first() is not None
