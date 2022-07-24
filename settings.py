fe765afe4fad5639a7f4204945de6db2c2e19642
Ivan Skvortsov <pprofcheg@gmail.com>
2022-07-24 13:19:28 +0300

    added default values to ENV vars in settings

--- settings.py
+++ settings.py
@@ -2,6 +2,6 @@ import os
 
 
 class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
+    SECRET_KEY = os.getenv('SECRET_KEY', default='YHjk*(#KF9398HJklfhskdjf8~!@&*)')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI', default='sqlite:///db.sqlite3')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
