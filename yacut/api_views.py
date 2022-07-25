2248140b2dc754823bef2f1dfc4a218fd0c45d58
Ivan Skvortsov <pprofcheg@gmail.com>
2022-07-25 12:36:21 +0300

    1st review fixes

--- yacut/api_views.py
+++ yacut/api_views.py
@@ -1,4 +1,5 @@
 import re
+from http import HTTPStatus
 
 from flask import jsonify, request
 
@@ -18,35 +19,47 @@ def create_id():
     url_map.deserialize(data)
     db.session.add(url_map)
     db.session.commit()
-    return jsonify(url_map.serialize(show_short=True)), 201
+    return jsonify(url_map.serialize(show_short=True)), HTTPStatus.CREATED
 
 
 @app.route('/api/id/<string:short_id>/', methods=['GET'])
 def get_url(short_id):
     url = URL_map.query.filter_by(short=short_id).first()
     if url is not None:
-        return jsonify(url.serialize()), 200
-    raise APIExceptionHandler('Указанный id не найден', 404)
+        return jsonify(url.serialize()), HTTPStatus.OK
+    raise APIExceptionHandler(
+        'Указанный id не найден', HTTPStatus.NOT_FOUND
+    )
 
 
 def validate_api_input(data):
     if data is None:
-        raise APIExceptionHandler('Отсутствует тело запроса', 400)
+        raise APIExceptionHandler(
+            'Отсутствует тело запроса', HTTPStatus.BAD_REQUEST
+        )
     if 'url' not in data:
-        raise APIExceptionHandler('\"url\" является обязательным полем!', 400)
+        raise APIExceptionHandler(
+            '\"url\" является обязательным полем!', HTTPStatus.BAD_REQUEST
+        )
     if not re.match(r'^https?://', data['url']):
-        raise APIExceptionHandler('Указан недопустимый url!', 400)
+        raise APIExceptionHandler(
+            'Указан недопустимый url!', HTTPStatus.BAD_REQUEST
+        )
 
     custom_id = data.get('custom_id')
     if not custom_id:
         return
     if not 1 < len(custom_id) < 16:
         raise APIExceptionHandler(
-            'Указано недопустимое имя для короткой ссылки', 400
+            'Указано недопустимое имя для короткой ссылки',
+            HTTPStatus.BAD_REQUEST
         )
     if not re.match(r'^[a-zA-Z0-9]+$', custom_id):
         raise APIExceptionHandler(
-            'Указано недопустимое имя для короткой ссылки', 400
+            'Указано недопустимое имя для короткой ссылки',
+            HTTPStatus.BAD_REQUEST
         )
     if short_id_exists_in_db(custom_id):
-        raise APIExceptionHandler(f'Имя "{custom_id}" уже занято.', 400)
+        raise APIExceptionHandler(
+            f'Имя "{custom_id}" уже занято.', HTTPStatus.BAD_REQUEST
+        )
--- yacut/views.py
+++ yacut/views.py
@@ -1,34 +1,33 @@
-from flask import Markup, render_template, flash, redirect, url_for, abort
+from flask import Markup, render_template, flash, redirect, url_for
 
 from yacut import app, db
 from yacut.forms import URLMapForm
 from yacut.models import URL_map
-from yacut.services import get_unique_short_id, short_id_exists_in_db
+from yacut.services import get_unique_short_id
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index_view():
     form = URLMapForm()
-    if form.validate_on_submit():
-        url_map = URL_map(
-            original=form.original_link.data,
-            short=form.custom_id.data or get_unique_short_id()
-        )
-        db.session.add(url_map)
-        db.session.commit()
-        link = url_for(
-            'redirect_view', custom_id=url_map.short, _external=True
-        )
-        message = Markup(
-            f'Ваша новая сслыка готова: <a href="{link}">{link}</a>'
-        )
-        flash(message)
+    if not form.validate_on_submit():
+        return render_template('index.html', form=form)
+    url_map = URL_map(
+        original=form.original_link.data,
+        short=form.custom_id.data or get_unique_short_id()
+    )
+    db.session.add(url_map)
+    db.session.commit()
+    link = url_for(
+        'redirect_view', custom_id=url_map.short, _external=True
+    )
+    message = Markup(
+        f'Ваша новая сслыка готова: <a href="{link}">{link}</a>'
+    )
+    flash(message)
     return render_template('index.html', form=form)
 
 
 @app.route('/<string:custom_id>', methods=['GET'])
 def redirect_view(custom_id):
-    if not short_id_exists_in_db(custom_id):
-        abort(404)
-    url_map = URL_map.query.filter_by(short=custom_id).first()
+    url_map = URL_map.query.filter_by(short=custom_id).first_or_404()
     return redirect(url_map.original)
